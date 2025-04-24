0f966e017b67ac6e65c906b63ea8bf3f4167eec4
Miroslav Jovic <jovicmiroslav88@gmail.com>
2025-04-25 00:02:35 +0200

    about text added

--- src/utils/data/aboutData.js
+++ src/utils/data/aboutData.js
@@ -68,7 +68,7 @@ export const aboutDataSkills = [
         title: "priznanja",
         info: [
             {
-                title: "Dugi niz godina rada doneo nam je veliki broj priznanja počevši od sportskih klubova, pa sve do mladenaca zadovoljnih svojim divnim svadbenim snimcima.",
+                title: "Dugi niz godina rada doneo nam je veliki broj priznanja počevši od sportskih klubova, pa sve do mladenaca zadovoljnih svojim divnim svadbenim snimcima. Jer danas, svaki događaj može biti sačuvan sa neverovatnom preciznošću i umetničkim dodirom. Ali šta je to što snimanje kamerom čini pravom umetnošću? Snimanje kamerom zahteva oko za detalje. Svaka scena, svaki pokret, i svaka emocija moraju biti uhvaćeni na način koji priča priču.",
             },
         ],
     },
@@ -76,7 +76,7 @@ export const aboutDataSkills = [
         title: "iskustvo",
         info: [
             {
-                title: "Ponosimo se dugogodišnjim iskustvom u snimanju i editovanju događaja - od svadbi do sportskih mečeva. Sa svakim novim zadatkom, donosimo svež pristup i kreativnost kako bismo stvorili video zapise koji ostavljaju trajan utisak.",
+                title: "Ponosimo se dugogodišnjim iskustvom u snimanju i editovanju događaja - od svadbi do sportskih mečeva. Sa svakim novim zadatkom, donosimo svež pristup i kreativnost kako bismo stvorili video zapise koji ostavljaju trajan utisak. Bez obzira da li se radi o suzi radosnici na venčanju ili energičnom skoku sportiste, pravi majstori kamere znaju kako da te trenutke uhvate u svom najboljem svetlu. Naravno, tehnika igra ključnu ulogu. Kvalitetna oprema omogućava snimanje u visokoj rezoluciji, sa savršenom jasnoćom i bojama.",
             },
         ],
     },
@@ -84,7 +84,7 @@ export const aboutDataSkills = [
         title: "Zašto Mi?",
         info: [
             {
-                title: "Kvalitet se prepoznaje na prvi pogled jer naša posvećenost pružanju vrhunskog kvaliteta u svakom kadru osigurava da svaki trenutak postane nezaboravan.",
+                title: "Kvalitet se prepoznaje na prvi pogled jer naša posvećenost pružanju vrhunskog kvaliteta u svakom kadru osigurava da svaki trenutak postane nezaboravan. Kroz pažljivo planiranje, oštro oko za detalje i veštinu montaže, svaki video može postati dragocena uspomena. Zato, sledeći put kada planirate neki važan događaj, setite se koliko je važno imati pravog umetnika iza kamere. Jer, na kraju dana, to su ti zabeleženi trenuci koji će ostati sa nama zauvek.",
             },
         ],
     },
