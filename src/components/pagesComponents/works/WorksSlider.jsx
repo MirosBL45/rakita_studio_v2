bdb72068eca1e7a593d23ecd3b85158b26b3241f
MirosBL45 <jovicmiroslav88@gmail.com>
2025-05-09 22:39:11 +0200

    work slider working

--- src/components/pagesComponents/works/WorksSlider.jsx
+++ src/components/pagesComponents/works/WorksSlider.jsx
@@ -1,4 +1,19 @@
-import React from 'react';
+// icon
+import { BsArrowRight } from 'react-icons/bs';
+
+// next image
+import Image from 'next/image';
+
+// links
+import Link from 'next/link';
+
+// swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
 
 export default function WorksSlider() {
   return <div>WorksSlider</div>;
---
+++ src/utils/data/workData.js
@@ -0,0 +1,55 @@
+// data
+import dimale from '@/assets/works/dimic-aleksandar-studio-rakita.jpg'
+
+export const workData = {
+    slides: [
+        {
+            images: [
+                {
+                    title: 'Aleksandar Dimić | Studio Rakita',
+                    path: dimale,
+                    link: 'https://www.youtube.com/watch?v=MMkDjw_c2XA',
+                },
+                {
+                    title: 'Zemunski kamerni orkestar | Studio Rakita',
+                    path: '/works/kamork.jpg',
+                    link: 'https://www.youtube.com/watch?v=oPS11rDEqYI',
+                },
+                {
+                    title: 'Prvenstvo Srbije gimnastika, grupne vežbe | Studio Rakita',
+                    path: '/works/svilajn.jpg',
+                    link: 'https://www.youtube.com/watch?v=6hhrdkUOp3c',
+                },
+                {
+                    title: 'Miodrag Dabižić | Studio Rakita',
+                    path: '/works/midrdab.jpg',
+                    link: 'https://www.youtube.com/watch?v=Opgk2dnG_5k',
+                },
+            ],
+        },
+        {
+            images: [
+                {
+                    title: 'Damjan Knežević | Studio Rakita',
+                    path: '/works/damkne.jpg',
+                    link: 'https://www.youtube.com/watch?v=W_EXpnaj750&t=130s',
+                },
+                {
+                    title: 'VK Banjica I VK Zemun | Studio Rakita',
+                    path: '/works/banzem.jpg',
+                    link: 'https://www.youtube.com/watch?v=I5nHwI_KwyM',
+                },
+                {
+                    title: 'Dragan Milojević | Studio Rakita',
+                    path: '/works/dramil.jpg',
+                    link: 'https://www.youtube.com/watch?v=11Ue-nPi6CU',
+                },
+                {
+                    title: 'FK Čukarički ACS JuniorUL | Studio Rakita',
+                    path: '/works/cukjun.jpg',
+                    link: 'https://www.youtube.com/watch?v=8cvYm5e_Gy8',
+                },
+            ],
+        },
+    ],
+};
